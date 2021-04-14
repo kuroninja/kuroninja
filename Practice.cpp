9a4e8ea4783d23d0547cb9d48051fe6cb0b0c9d2
kuroninja <81969259+kuroninja@users.noreply.github.com>
2021-04-14 09:40:06 -0400

    Add files via upload

---
+++ Practice.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// This is the first function that runs
+int main() {
+    std::string s;
+    std::cout << "Enter input: " << "\n";
+    std::cin >> s;
+    std::cout << s;
+}
