1f8ba829918253d2a0c5dd8a19861d2cc496e8d6
kuroninja <81969259+kuroninja@users.noreply.github.com>
2021-04-14 09:40:18 -0400

    Add files via upload

---
+++ New Practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age;
+    age = 24;
+
+    double accountBalance = 9999.99
+
+    bool isGameOVer = false;
+    isGameOver = true;
+
+    std::string name = "Julian";
+    
+}
