feb24b39f544c61a7f70475ecfef8f0f2c3e65da
kuroninja <81969259+kuroninja@users.noreply.github.com>
2021-04-14 09:40:37 -0400

    Add files via upload

---
+++ arrayspractice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // std::string roster[] = {"Julian", "Boolian", "Draco", "Lisa"}
+    // std::string firstName = roster roster[0];
+    // roster[0] = "Johny";
+
+    std::string roster[10];
+    roster[0] = "Sally";
+    roster[1] = "Jack";
+}
