4e625da65657b4ccc4e70ce77a7d139ca9d73c91
kuroninja <81969259+kuroninja@users.noreply.github.com>
2021-04-15 14:52:03 -0400

    Create hellyeah.cpp

---
+++ hellyeah.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class User {
+    public:
+    std::string name;
+    int age;
+    bool isLoggedIn;
+
+
+    User(std::string n, int a) {
+        name = n;
+        age = a;
+        isLoggedIn = true;
+    }
+
+    void changeLoginStatus() {
+        isLoggedIn = !isLoggedIn;
+    }
+};
+
+int main() {
+    User user = User("Julian", 24);
+    User user2 = User("Nimish", 27);
+    std::cout << user.name;
+    std::cout << user.age;
+    std::cout << user.isLoggedIn;
+    user.changeLoginStatus();
+    std::cout << user.isLoggedIn;
+}
