7496c0fba0fd5d0054c50c1e16fc416ea52ad1cd
kuroninja <81969259+kuroninja@users.noreply.github.com>
2021-04-16 08:48:36 -0400

    Delete Practice.cpp

--- Practice.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-// This is the first function that runs
-int main() {
-    std::string s;
-    std::cout << "Enter input: " << "\n";
-    std::cin >> s;
-    std::cout << s;
-}
